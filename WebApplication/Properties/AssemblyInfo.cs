784ad1685bb356de01e8f97dee4aec8efd7ac566
ngiakhanh <ngiakhanh96@gmail.com>
2018-07-06 00:30:40 +0700

    nothing

--- WebApplication/Properties/AssemblyInfo.cs
+++ WebApplication/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("530b6560-50e2-4666-9843-aab101a9d4f5")]
+[assembly: Guid("98ac051f-5ae5-4edc-bc82-a1eb48927556")]
 
 // Version information for an assembly consists of the following four values:
 //
